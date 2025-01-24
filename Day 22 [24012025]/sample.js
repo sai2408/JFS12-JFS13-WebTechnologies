332d2bef57ac7be114da774844fe0453618ac02a
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2025-01-24 10:50:46 +0530

    Add files via upload

---
+++ Day 22 [24012025]/sample.js
@@ -0,0 +1,29 @@
+//Variables
+
+//var
+/*
+var a = 10;
+console.log(a);
+var a = 20;
+console.log(a);
+a = 200;
+console.log(a);
+*/
+//let
+/*
+let a = 10;
+console.log(a);
+a = 16;
+console.log(a);
+let a = 30;
+console.log(a);
+*/
+//const
+/*
+const a = 100;
+console.log(a);
+const a = 200;
+console.log(a);
+a = 200;
+console.log(a);
+*/
