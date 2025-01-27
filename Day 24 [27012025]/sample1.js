c80dfc361193b65637e8a5df7fb4182cdca50825
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2025-01-27 10:56:23 +0530

    Add files via upload

---
+++ Day 24 [27012025]/sample1.js
@@ -0,0 +1,61 @@
+//Comparision Operators
+/*
+console.log("3" == 2);
+console.log("3" == 3);
+console.log("3" === 2);
+console.log("3" === 3);
+console.log("3" === "3");
+console.log("3" != 2);
+console.log("3" != 3);
+console.log("3" !== 3);
+console.log("3" !== "2")
+console.log(10>9);
+console.log(10>=10);
+console.log(10<10);
+console.log(10<16);
+console.log(10<=10);
+*/
+//Logical Operators
+/*
+console.log((10>6) && (10>5));
+console.log((10>16) && (10>5));
+console.log((10>6) && (10>15));
+console.log((10>6) || (10>5));
+console.log((10>16) || (10>5));
+console.log((10>6) || (10>15));
+console.log(!(10>19));
+*/
+//Unary Operator
+/*
+var a = "10";
+console.log(+a);
+console.log(-a);
+*/
+//Ternary Operator
+/*
+var x = (10>2) ? "hello" : "world";
+console.log(x);
+var status = (18>=18) ? "adult" : "minor";
+console.log(status);
+*/
+//Inc - Dec Operators
+/*
+var a = 10;
+console.log(a++);
+console.log(a);
+var a = 10
+console.log(++a);
+console.log(a);
+var q = 10;
+console.log(q--);
+console.log(q);
+var q = 10;
+console.log(--q);
+console.log(q);
+*/
+//String Concatenation operator
+/*
+var a  = "Sai";
+var b = "Vardhan";
+console.log(a+" "+b);
+*/
