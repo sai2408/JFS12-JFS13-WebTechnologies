d5d7310cf2c685288c55ce964ce5afde575c7054
Venkata sai vardhan Thimmisetty <57175225+sai2408@users.noreply.github.com>
2025-01-28 10:44:40 +0530

    Add files via upload

---
+++ Day 25 [28012025]/sample1.js
@@ -0,0 +1,70 @@
+//If Statement
+/*
+var x = 20;
+if (x%2==0){
+    console.log("Even");
+}*/
+//IF Else
+/*
+var n = 3
+var k = 4
+var ts = n * k
+if (ts%4==0){
+    console.log(ts/4);
+}
+else{
+    var res = ts/4;
+    res = parseInt(res);
+    console.log(res+1);
+}
+*/
+//IF ELSE If ElSE
+/*
+var x = 13;
+if (x < 0){
+    console.log("x value is negetive");
+}
+else if(x%2==0){
+    console.log("Even Number");
+}
+else{
+    console.log("Odd number");
+}
+*/
+//Switch
+/*
+var x = 4;
+switch (x){
+    case 1:
+        console.log("One");
+        break 
+    case 2:
+        console.log("Two");
+        break 
+    case 3:
+        console.log("Three");
+        break 
+    default:
+        console.log("Number not exist");
+}
+*/
+// Nested Consitional Statements
+/*
+var x = -14;
+if (x>0){
+    if (x%2==0){
+        console.log("Positive Even Number");
+    }
+    else{
+        console.log("Positive Odd number");
+    }
+}
+else{
+    if (x%2 == 0){
+        console.log("Negetive Even Number");
+    }
+    else{
+        console.log("Negetive Odd Number");
+    }
+}
+*/
